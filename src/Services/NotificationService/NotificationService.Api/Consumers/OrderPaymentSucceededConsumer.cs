c165aa6a75db585a8125c2955a8e238bc2926cd3
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-03-12 17:03:18 +0300

    add NotificationService.Api

---
+++ src/Services/NotificationService/NotificationService.Api/Consumers/OrderPaymentSucceededConsumer.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using PaymentService.Messages.Events;
+
+namespace NotificationService.Api.Consumers
+{
+    public class OrderPaymentSucceededConsumer : IConsumer<OrderPaymentSucceeded>
+    {
+        private readonly ILogger<OrderPaymentSucceededConsumer> _logger;
+
+        public OrderPaymentSucceededConsumer(ILogger<OrderPaymentSucceededConsumer> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task Consume(ConsumeContext<OrderPaymentSucceeded> context)
+        {
+            _logger.LogDebug("OrderPaymentSucceededConsumer-OrderId: {OrderId} PaymentId: {PaymentId}",
+                context.Message.OrderId,
+                context.Message.PaymentId);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/NotificationService/NotificationService.Api/Program.cs
@@ -0,0 +1,30 @@
+using SelenyumMicroService.MessageService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var messageServiceConnectionSettings = new MessageServiceConnectionSettings();
+builder.Configuration.GetSection("RabbitMQ").Bind(messageServiceConnectionSettings);
+builder.Services.AddSingleton(messageServiceConnectionSettings);
+builder.Services.AddMessageService(messageServiceConnectionSettings);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
