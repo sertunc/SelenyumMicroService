1d7eb2ca7bb3c162de8d4ef3ba3b4a377b4d437d
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-03-19 16:23:01 +0300

    implement ocelot on IdentityService.Api

--- src/Services/IdentityService/IdentityService.Api/Program.cs
+++ src/Services/IdentityService/IdentityService.Api/Program.cs
@@ -1,7 +1,10 @@
 using IdentityService.Business.Business;
 using IdentityService.Business.Abstractions.Interfaces;
+using SelenyumMicroService.ServiceDiscovery.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
+var urls = builder.Configuration.GetValue<string>("Urls") ?? Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+ArgumentNullException.ThrowIfNull(urls);
 
 // Add services to the container.
 builder.Services.AddScoped<IIdentityService, CustomIdentityService>();
@@ -9,6 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var consulAddress = builder.Configuration["Consul:Address"];
+ArgumentNullException.ThrowIfNull(consulAddress);
+var consulSettings = new ConsulSettings
+{
+    ConsulAddress = consulAddress,
+    ServiceId = "IdentityService",
+    ServiceName = "IdentityService",//Web.ApiGateway->/Configurations/ocelot.json -> ServiceName
+    ServiceHostUrl = urls,
+    Tags = [Environment.MachineName]
+};
+builder.Services.AddConsul(consulSettings);
 
 var app = builder.Build();
 
@@ -20,9 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+app.UseConsul(app.Lifetime, consulSettings);
 
-app.Run();
+app.Run(urls);
