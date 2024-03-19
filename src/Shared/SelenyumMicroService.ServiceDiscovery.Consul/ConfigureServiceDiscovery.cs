a1856b2d714e56d55502a7f2eb84cf48d15fde7b
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-03-19 16:11:49 +0300

    add SelenyumMicroService.ServiceDiscovery.Consul

---
+++ src/Shared/SelenyumMicroService.ServiceDiscovery.Consul/ConfigureServiceDiscovery.cs
@@ -0,0 +1,57 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SelenyumMicroService.ServiceDiscovery.Consul
+{
+    public static class ConfigureServiceDiscovery
+    {
+        public static IServiceCollection AddConsul(this IServiceCollection services, ConsulSettings consulSettings)
+        {
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                ArgumentNullException.ThrowIfNull(consulSettings.ConsulAddress);
+
+                consulConfig.Address = new Uri(consulSettings.ConsulAddress);
+            }));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app, IHostApplicationLifetime lifetime, ConsulSettings consulSettings)
+        {
+            ArgumentNullException.ThrowIfNull(consulSettings.ServiceId);
+            ArgumentNullException.ThrowIfNull(consulSettings.ServiceName);
+            ArgumentNullException.ThrowIfNull(consulSettings.ServiceHostUrl);
+
+            var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+
+            var loggingFactory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            var logger = loggingFactory.CreateLogger<IApplicationBuilder>();
+
+            //Register service with consul
+            var registration = new AgentServiceRegistration()
+            {
+                ID = $"{consulSettings.ServiceId}_{new Uri(consulSettings.ServiceHostUrl).Port}",
+                Name = consulSettings.ServiceName,
+                Address = new Uri(consulSettings.ServiceHostUrl).Host,
+                Port = new Uri(consulSettings.ServiceHostUrl).Port,
+                Tags = consulSettings.Tags,
+            };
+
+            logger.LogInformation("Registering with Consul");
+            consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+            consulClient.Agent.ServiceRegister(registration).Wait();
+
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                logger.LogInformation("Deregistering from Consul");
+                consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/Shared/SelenyumMicroService.ServiceDiscovery.Consul/ConsulSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace SelenyumMicroService.ServiceDiscovery.Consul
+{
+    public class ConsulSettings
+    {
+        public string ConsulAddress { get; set; }
+        public string ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceHostUrl { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
