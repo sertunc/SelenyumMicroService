05272f2a17880632028c3ee151e7fdd001c0a812
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-03-19 16:18:02 +0300

    implement ocelot on CatalogService.Api

--- src/ApiGateways/WebApiGateway/Web.ApiGateway/Program.cs
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Program.cs
@@ -1,10 +1,11 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration).AddConsul();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/Services/CatalogService/CatalogService.Api/Program.cs
+++ src/Services/CatalogService/CatalogService.Api/Program.cs
@@ -2,14 +2,29 @@ using CatalogService.Business.Mapping;
 using CatalogService.Business.Extensions;
 using CatalogService.Data.EFCore.Extensions;
 using SelenyumMicroService.Bootstrapper;
+using SelenyumMicroService.ServiceDiscovery.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
+var urls = builder.Configuration.GetValue<string>("Urls") ?? Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+ArgumentNullException.ThrowIfNull(urls);
 
 // Add services to the container.
 builder.Services.AddAutoMapper<MappingProfile>();
 builder.Services.AddDbContextConfigurations(builder.Configuration);
 builder.Services.AddDbContextRepositories();
 builder.Services.AddBusiness();
+
+var consulAddress = builder.Configuration["Consul:Address"];
+ArgumentNullException.ThrowIfNull(consulAddress);
+var consulSettings = new ConsulSettings
+{
+    ConsulAddress = consulAddress,
+    ServiceId = "CatalogService",
+    ServiceName = "CatalogService",//Web.ApiGateway->/Configurations/ocelot.json -> ServiceName
+    ServiceHostUrl = urls,
+    Tags = [Environment.MachineName]
+};
+builder.Services.AddConsul(consulSettings);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,9 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+app.UseConsul(app.Lifetime, consulSettings);
 
-app.Run();
+app.Run(urls);
