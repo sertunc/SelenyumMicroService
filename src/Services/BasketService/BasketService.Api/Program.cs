376308a425ad77f547949e6f6acf6c52eef54ed3
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-03-20 11:26:24 +0300

    implement ocelot on BasketService.Api

--- src/Services/BasketService/BasketService.Api/Program.cs
+++ src/Services/BasketService/BasketService.Api/Program.cs
@@ -1,11 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using SelenyumMicroService.ServiceDiscovery.Consul;
 
+var builder = WebApplication.CreateBuilder(args);
+var urls = builder.Configuration.GetValue<string>("Urls") ?? Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+ArgumentNullException.ThrowIfNull(urls);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var consulAddress = builder.Configuration["Consul:Address"];
+ArgumentNullException.ThrowIfNull(consulAddress);
+var consulSettings = new ConsulSettings
+{
+    ConsulAddress = consulAddress,
+    ServiceId = "BasketService",
+    ServiceName = "BasketService",//Web.ApiGateway->/Configurations/ocelot.json -> ServiceName
+    ServiceHostUrl = urls,
+    Tags = [Environment.MachineName]
+};
+builder.Services.AddConsul(consulSettings);
 
 var app = builder.Build();
 
@@ -22,4 +37,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.UseConsul(app.Lifetime, consulSettings);
+
+app.Run(urls);
