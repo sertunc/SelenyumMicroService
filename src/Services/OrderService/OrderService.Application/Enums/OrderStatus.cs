37e685c1680b6f8ae3dd5d433cf486f9536cc813
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-04-02 09:57:00 +0300

    Create OrderStatus.cs

---
+++ src/Services/OrderService/OrderService.Application/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderService.Application.Enums
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Shipped,
+        Delivered,
+        Canceled
+    }
+}
