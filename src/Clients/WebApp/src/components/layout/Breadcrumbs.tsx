df190d56d71965010f29694c4c527dcfcfdd00db
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-04-05 16:21:28 +0300

    add breadcrumb

--- src/Clients/WebApp/src/components/layout/Breadcrumbs.tsx
+++ src/Clients/WebApp/src/components/layout/Breadcrumbs.tsx
@@ -1,46 +1,106 @@
-import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import CommonStyles from "../../CommonStyles";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
-import HomeIcon from "@mui/icons-material/Home";
-import WhatshotIcon from "@mui/icons-material/Whatshot";
-import GrainIcon from "@mui/icons-material/Grain";
+import { Link } from "react-router-dom";
+import { BreadcrumbItemModel } from "../../models/BreadcrumbItemModel";
 
-function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-  event.preventDefault();
-  console.info("You clicked a breadcrumb.");
-}
+type CatalogInfo = [string, string];
 
 export default function IconBreadcrumbs() {
+  const location = useLocation();
+
+  const [items, setItems] = useState<BreadcrumbItemModel[]>([
+    {
+      label: "Home",
+      link: "/",
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(location.pathname);
+    determinePage(location.pathname);
+  }, [location]);
+
+  const determinePage = (pathname: string) => {
+    const items = [
+      {
+        label: "Home",
+        link: "/",
+      },
+    ];
+
+    switch (true) {
+      case pathname.startsWith("/catalogdetail"):
+        const detailId = pathname.split("/")[2];
+        const detailCatalogInfo = getCatalogInfo(detailId);
+
+        items.push({
+          label: detailCatalogInfo[0],
+          link: "#",
+        });
+
+        items.push({
+          label: detailCatalogInfo[1],
+          link: "#",
+        });
+        break;
+      case pathname.startsWith("/catalog"):
+        const catalogId = pathname.split("/")[2];
+        items.push({
+          label: getCatalogCategory(catalogId),
+          link: "#",
+        });
+        break;
+      default:
+        console.log("Bu farklı bir sayfadır.");
+    }
+
+    setItems(items);
+  };
+
+  //TODO: maybe we can get from graphdb
+  const getCatalogCategory = (id: string) => {
+    switch (id) {
+      case "1":
+        return "Mug";
+      case "2":
+        return "T-Shirt";
+      case "3":
+        return "Sheet";
+      case "4":
+        return "USB Memory Stick";
+      default:
+        return "Home";
+    }
+  };
+
+  //TODO: maybe we can get from graphdb
+  const getCatalogInfo = (catologId: string): CatalogInfo => {
+    switch (catologId) {
+      case "1":
+        return ["Mug", "Self Stirring Coffee Mug"];
+      case "2":
+        return ["Mug", "Aardman Wallace Mug"];
+      case "5":
+        return ["T-Shirt", "Gildan Men's Crew T-Shirts"];
+      case "8":
+        return ["Sheet", "Microfiber 4-Piece Bed Sheet"];
+      default:
+        return ["Home", "Home"];
+    }
+  };
+
   return (
-    <div role="presentation" onClick={handleClick}>
+    <>
       <h3 />
       <Breadcrumbs aria-label="breadcrumb">
-        <Link
-          underline="hover"
-          sx={{ display: "flex", alignItems: "center" }}
-          color="inherit"
-          href="/"
-        >
-          <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Home
-        </Link>
-        <Link
-          underline="hover"
-          sx={{ display: "flex", alignItems: "center" }}
-          color="inherit"
-          href="/material-ui/getting-started/installation/"
-        >
-          <WhatshotIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Catalog List
-        </Link>
-        {/* <Typography
-          sx={{ display: "flex", alignItems: "center" }}
-          color="text.primary"
-        >
-          <GrainIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Breadcrumb
-        </Typography> */}
+        {items.map((item, index) => (
+          <Link key={index} style={CommonStyles.link} to={item.link}>
+            {item.label}
+          </Link>
+        ))}
       </Breadcrumbs>
-    </div>
+    </>
   );
 }
